ad5a4eea12a4719cbd06910f0bd509e70ff30bde
Gurbeer-Sokhi <gsokhi@stevens.edu>
2023-10-08 00:11:56 -0400

    project

---
+++ src/main/java/Divise/DiviseCompute/DiviseComputeApplication.java
@@ -0,0 +1,13 @@
+package Divise.DiviseCompute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiviseComputeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiviseComputeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Divise/DiviseCompute/DiviseComputeApplicationTests.java
@@ -0,0 +1,13 @@
+package Divise.DiviseCompute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiviseComputeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
